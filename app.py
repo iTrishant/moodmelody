63adc9c42755bed34dcf5aa60db977c4c3dcb670
Trishant Dudi <138218901+iTrishant@users.noreply.github.com>
2024-07-16 02:30:23 +0530

    Updated sp_oauth

--- app.py
+++ app.py
@@ -37,6 +37,12 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                open_browser=False,
                                                cache_path="./.spotifycache"))
 
+sp_oauth = SpotifyOAuth(client_id=CLIENT_ID,
+                        client_secret=CLIENT_SECRET,
+                        redirect_uri=REDIRECT_URI,
+                        scope="user-modify-playback-state user-read-playback-state",
+                        cache_path="./.spotifycache")
+
 # Define song URIs for each emotion
 emotion_to_song_uri = {
     'anger': 'spotify:track:7iN1s7xHE4ifF5povM6A48',    # let it be
